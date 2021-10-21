6175dd1422e4927b2825ca114c6a8d440b2a33d0
Marcelo Vieira <80363188+marcelo-vieira-uninove@users.noreply.github.com>
2021-10-21 15:38:30 -0300

    Create faca-parte.component.ts

---
+++ src/app/components/faca-parte/faca-parte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'faca-parte',
+  templateUrl: './faca-parte.component.html',
+  styleUrls: ['./faca-parte.component.scss']
+})
+export class FacaParteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
