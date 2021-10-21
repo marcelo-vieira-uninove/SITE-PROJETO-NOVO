8ba3f8d22a66ba26fae893dd0f124ad0e8740d92
Marcelo Vieira <80363188+marcelo-vieira-uninove@users.noreply.github.com>
2021-10-21 15:39:20 -0300

    Create imagem.component.ts

---
+++ src/app/components/imagem/imagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'imagem',
+  templateUrl: './imagem.component.html',
+  styleUrls: ['./imagem.component.scss']
+})
+export class ImagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
