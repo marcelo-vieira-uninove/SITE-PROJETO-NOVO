3c09fb95a83f44426f165575f1fd2bab845c6597
Marcelo Vieira <80363188+marcelo-vieira-uninove@users.noreply.github.com>
2021-10-21 15:41:17 -0300

    Create onde-assistir.component.ts

---
+++ src/app/components/onde-assistir/onde-assistir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'onde-assistir',
+  templateUrl: './onde-assistir.component.html',
+  styleUrls: ['./onde-assistir.component.scss']
+})
+export class OndeAssistirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
